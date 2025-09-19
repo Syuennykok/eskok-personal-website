9f65906a58ac25cb9489c830320c5a59eef9674a
Syuennykok <syuennykok@gmail.com>
2025-09-19 10:02:56 +0800

    Quartz sync: Sep 19, 2025, 10:02 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Hello",
+    pageTitle: "Syuen's Brain",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
